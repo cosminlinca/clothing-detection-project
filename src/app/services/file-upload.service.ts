6a1832e9289e42fd62e3772c38830356064f9acd
Cosmin Linca <cosmin_linca@outlook.com>
2022-01-03 20:22:00 +0200

    localhost

--- src/app/services/file-upload.service.ts
+++ src/app/services/file-upload.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class FileUploadService {
-  private baseUrl = 'http://188.24.124.168:5000';
+  private baseUrl = 'http://localhost:5000';
 
   constructor(private http: HttpClient) { }
 
